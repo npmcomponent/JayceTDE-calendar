2daef42c48e5fa0e5b3332ac9dc95101f41bae03
Jayce Pulsipher <jaycemp@gmail.com>
2013-06-07 10:20:07 -0600

    0.0.1 release

--- Readme.md
+++ Readme.md
@@ -1,16 +1,75 @@
 
 # calendar
 
-  A plugable calendar
+  A basic calendar
 
 ## Installation
 
     $ component install JayceTDE/calendar
 
+## Example
+
+```js
+
+var Calendar = require('calendar')
+  , cal = new Calendar({
+    date: '2000/05/05',
+    headLength: 5
+  })
+;
+
+document.body.appendChild(cal.el);
+
+```
+
+## Events
+
+    - `render` () when the calendar is rendered
+    - `render head` () when the days of the week are rendered
+    - `change` (date) when there is any change to the date of the calendar
+    - `change month` (month) when the month of the calendar is changed
+    - `change year` (year) when the year of the calendar is changed
+
 ## API
 
-   
+### new Calendar(options)
 
+    Initialize a new Calendar
+
+#### options.date
+
+    Initial date to render. defaults to `new Date`
+
+#### options.headLength
+
+    Length of the week day names. defaults to `2` (ex: 'Sunday' -> 'Su')
+
+### Calendar#setDate(date)
+
+    Render the calendar for the given `date`. If a string is used, it will be parsed with `new Date(str)`
+    
+### Calendar#setMonth(month)
+
+    Set the `month` of the calendar's current date and re-render. 0 - January, 11 - December
+
+### Calendar#setYear(year)
+
+    Set the `year` of the calendar's current date and re-render.
+    
+### Calendar#nextMonth()
+### Calendar#prevMonth()
+### Calendar#nextYear()
+### Calendar#prevYear()
+
+### Calendar#render()
+
+    Renders the calendar. Called internally when the date is changed using the above methods
+    
+### Calendar#renderHead()
+
+    Renders the days of the week. Called when initialized.
+    To change the length each day, change Calendar#options.headLength, and call Calendar#renderHead()
+    
 ## License
 
   MIT
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+'use strict';
+
+var Emitter = require('emitter')
+  , inGroupsOf = require('in-groups-of')
+  , templateEl = document.createElement('table')
+;
+
+// Setup template
+templateEl.className = 'calendar-table';
+templateEl.innerHTML = '<thead></thead><tbody></tbody>';
+
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   , months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   , daysInMonthArr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -30,3 +41,154 @@ function isLeapYear(year) {
       || (0 === year)
     ;
 }
+
+function dayHTML(year, month, day, className) {
+    return '<td data-date="' + [year, month, day].join('-') + '"' + (className !== undefined ? ' class="' + className + '"' : '') + '><div class="day">' + day + '</div></td>';
+}
+
+function Calendar(options) {
+    
+    Emitter.call(this);
+    
+    this.options = options || {};
+    
+    // construct el
+    this.el = templateEl.cloneNode(true);
+    this._head = this.el.querySelector('thead');
+    this._body = this.el.querySelector('tbody');
+    
+    this.renderHead();
+    this.setDate(this.options.date || new Date());
+    
+}
+
+Emitter(Calendar.prototype);
+
+Calendar.prototype.renderHead = function (len) {
+    
+    var self = this;
+    
+    this._head.innerHTML = '<tr class="subheading">' + days.map(function(day){
+        return '<th>' + day.slice(0, self.options.headLength || 2) + '</th>';
+    }).join('') + '</tr>';
+    
+    return this.emit('render head');
+    
+};
+
+Calendar.prototype.render = function () {
+    
+    var start = new Date(this._date)
+      , month = start.getMonth()
+      , year = start.getFullYear()
+      , inMonth = daysInMonth(month, year)
+    ;
+    
+    start.setDate(1); // Should already be the first day if using setter
+    
+    var beforeDate = new Date(start)
+      , daysBefore = start.getDay()
+      , total = 7 * Math.ceil((inMonth + daysBefore) / 7)
+      , afterDate = new Date(start)
+      , daysAfter = total - (inMonth + daysBefore)
+      , afterDay = 0
+    ;
+    
+    beforeDate.setMonth(month - 1);
+    afterDate.setMonth(month + 1);
+    
+    var beforeDays = daysInMonth(beforeDate.getMonth(), beforeDate.getFullYear())
+      , afterDays = daysInMonth(afterDate.getMonth(), afterDate.getFullYear())
+      , cells = []
+      , rows
+      , day = 0
+    ;
+    
+    while (daysBefore--) {
+        cells.unshift(dayHTML(beforeDate.getFullYear(), beforeDate.getMonth(), beforeDays--, 'before-day'));
+    }
+    
+    while (day++ < inMonth) {
+        cells.push(dayHTML(year, month, day));
+    }
+    
+    while (daysAfter--) {
+        cells.push(dayHTML(afterDate.getFullYear(), afterDate.getMonth(), ++afterDay, 'after-day'))
+    }
+    
+    rows = inGroupsOf(cells, 7);
+    
+    this._body.innerHTML = rows.map(function (row) {
+        return '<tr>' + row.join('') + '</tr>';
+    }).join('');
+    
+    return this.emit('render', this._date);
+    
+};
+
+Calendar.prototype.setDate = function (date) {
+    
+    // Private instance of the supplied date. Will parse strings if passed
+    date = new Date(date);
+    
+    date.setDate(1); // Prevent overflowing month
+    this._date = date;
+    
+    this
+      .emit('change year', date.getFullYear())
+      .emit('change month', date.getMonth())
+      .emit('change', date);
+    
+    return this.render();
+};
+
+Calendar.prototype.setMonth = function (month) {
+    
+    var prevYear = this._date.getFullYear()
+      , year
+    ;
+    
+    this._date.setMonth(month);
+    
+    year = this._date.getFullYear();
+    
+    if (prevYear !== year) {
+        this.emit('change year', year);
+    }
+    
+    this
+      .emit('change month', this._date.getMonth())
+      .emit('change', this._date)
+    ;
+    
+    return this.render();
+};
+
+Calendar.prototype.setYear = function (year) {
+    this._date.setFullYear(year);
+    
+    this
+      .emit('change year', year)
+      .emit('change', this._date)
+    ;
+    
+    return this.render();
+};
+
+Calendar.prototype.nextMonth = function () {
+    return this.setMonth(this._date.getMonth() + 1);
+};
+
+Calendar.prototype.prevMonth = function () {
+    return this.setMonth(this._date.getMonth() - 1);
+};
+
+Calendar.prototype.nextYear = function () {
+    return this.setYear(this._date.getFullYear() + 1);
+};
+
+Calendar.prototype.prevYear = function () {
+    return this.setYear(this._date.getFullYear() - 1);
+};
+
+module.exports = Calendar;
